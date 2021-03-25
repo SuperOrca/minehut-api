3188fc7080149dd3c3fe45f8875b08720ae11b32
SuperOrca <69650371+SuperOrca@users.noreply.github.com>
2021-03-25 13:31:15 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open('README.md', 'r', encoding='utf-8') as README:
+    long_description = README.read()
+
+setuptools.setup(
+    name='owoify-py',
+    version='1.0.0',
+    author='SuperOrca',
+    description='Minehut API wrapper',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/deadshot465/owoify-py',
+    packages=setuptools.find_packages(),
+    classfiers=[
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
+    ],
+    python_requires='>=3.6'
+)
