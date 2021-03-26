89a063c295ea85491315eef44e3f2e4b8d68192d
SuperOrca <69650371+SuperOrca@users.noreply.github.com>
2021-03-26 12:47:41 -0400

    :construction: Fixed circular import

--- minehut/__init__.py
+++ minehut/__init__.py
@@ -1,4 +1,4 @@
-from .manager import getCredentials
-from .network import getServerCap, getTop5, getServerCount, getPlayerCount
 from .plugin import getPlugin, getPlugins
 from .server import getServer, getServers
+from .network import getServerCap, getTop5, getServerCount, getPlayerCount
+from .manager import getCredentials
