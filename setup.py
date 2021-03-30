b72194f9ca05fce16bcc8ecc05ed3c31aec61a49
SuperOrca <69650371+SuperOrca@users.noreply.github.com>
2021-03-30 16:37:00 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,32 @@
 import setuptools
+from pip.download import PipSession
+from pip.req import parse_requirements
 
 with open('README.md', 'r', encoding='utf-8') as README:
     long_description = README.read()
 
+install_reqs = parse_requirements('requirements.txt', session=PipSession())
+reqs = [str(ir.req) for ir in install_reqs]
+version = '1.0.0'
+
 setuptools.setup(
     name='minehut',
-    version='1.0.0',
+    version=version,
+    install_requires=reqs,
+    include_package_data=True,
     author='SuperOrca',
+    license='MIT',
     description='Minehut API wrapper',
+    keywords=['minehut', 'minehut api'],
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/SuperOrca/minehut-api',
     packages=setuptools.find_packages(),
     classfiers=[
-        'Programming Language :: Python :: 3.6',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent'
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6'
     ],
     python_requires='>=3.6'
 )
